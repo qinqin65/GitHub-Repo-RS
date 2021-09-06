c64ff8b9b481b2f0188232aba8ce8a0d14b8c5d1
qinqin65 <tom163com@126.com>
2021-09-06 23:04:03 +1000

    add new database for recording updating status

--- db_manager.py
+++ db_manager.py
@@ -6,5 +6,6 @@ client = pymongo.MongoClient(cfg.db_conn_str, serverSelectionTimeoutMS=5000)
 db = client.github_project
 users = db.users_3
 repositories = db.repositories_3
+update_status = db.update_status
 
 close = client.close
