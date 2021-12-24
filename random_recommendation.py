b7a7e23feb5a625ddda35367c3f68d13a8e61031
qinqin65 <s3805989@student.rmit.edu.au>
2021-12-24 06:59:29 +0000

    group metrics code

--- random_recommendation.py
+++ random_recommendation.py
@@ -1,47 +1,107 @@
 import numpy as np
 import pickle
+from util import Group
 
 def top_k_evaluate(top_k, rating_matrix, train_data, test_data):
     users_count = rating_matrix.shape[0]
     repos_count = rating_matrix.shape[1]
+    # hit rate
     hit_rates = np.zeros(users_count)
-    group_0_5 = []
-    group_5_10 = []
-    group_10_15 = []
-    group_15_over = []
+    group_hit_rate = {}
+    hit_rate_groups = Group()
+
+    # MRR
+    mrr = np.zeros(users_count)
+    group_mrr = {}
+    mrr_groups = Group()
+
+    # nDCG
+    ndcg = np.zeros(users_count)
+    group_ndcg = {}
+    ndcg_groups = Group()
+
+    interaction_matrix = pickle.load(open('data/interaction_matrix.p', 'rb'))
+    user_repos = np.sum(interaction_matrix, axis=1)
+    user_1_repo = np.where(user_repos==1)[0]
 
     for i, rating in enumerate(rating_matrix):
         non_train_mask = np.invert(train_data[i] > 0)
         recommendation = np.random.choice(np.where(non_train_mask)[0], size=top_k, replace=False)
         ground_truth = np.where(test_data[i]>0)[0]
 
-        intersections = np.intersect1d(recommendation, ground_truth)
-        hit_rate = -1 if len(ground_truth) == 0 else len(intersections) / min(len(ground_truth), top_k)
+        intersections, recommendation_index, ground_truth_index = np.intersect1d(recommendation, ground_truth, return_indices=True)
+        number_of_ground_truth = len(ground_truth)
+        number_of_intersections = len(intersections)
+
+        hit_rate = -1 if number_of_ground_truth == 0 else number_of_intersections / min(number_of_ground_truth, top_k)
         hit_rates[i] = hit_rate
+
+        # MRR
+        if number_of_intersections > 0:
+            if recommendation_index[0] <= top_k:
+                mrr[i] = 1 / (recommendation_index[0] + 1)
+        elif number_of_ground_truth == 0:
+            mrr[i] == -1
+        
+        # nDCG
+        if number_of_ground_truth == 0:
+            ndcg[i] = -1
+        else:
+            relevance_score = test_data[i][recommendation][:min(number_of_ground_truth, top_k)]
+            relevance_score_idea = test_data[i][ground_truth][:min(number_of_ground_truth, top_k)]
+            pow_rel = np.power(2, relevance_score) - 1
+            pow_rel_idea = np.power(2, relevance_score_idea) - 1
+            ranks = np.arange(start=1, stop=len(relevance_score) + 1)
+            log_rank = np.log2(ranks + 1)
+            dcg = np.sum(pow_rel / log_rank)
+            idcg = np.sum(pow_rel_idea / log_rank)
+            ndcg[i] = dcg / idcg
 
         # grouping
         repos_count = len(test_data[i][test_data[i]>0])
         if repos_count < 5:
-            group_0_5.append(i)
+            hit_rate_groups['0-5'].append(i)
+            mrr_groups['0-5'].append(i)
+            ndcg_groups['0-5'].append(i)
+            if repos_count == 1 and i in user_1_repo:
+                hit_rate_groups['1'].append(i)
+                mrr_groups['1'].append(i)
+                ndcg_groups['1'].append(i)
         elif repos_count < 10:
-            group_5_10.append(i)
+            hit_rate_groups['5-10'].append(i)
+            mrr_groups['5-10'].append(i)
+            ndcg_groups['5-10'].append(i)
         elif repos_count < 15:
-            group_10_15.append(i)
+            hit_rate_groups['10-15'].append(i)
+            mrr_groups['10-15'].append(i)
+            ndcg_groups['10-15'].append(i)
         else:
-            group_15_over.append(i)
+            hit_rate_groups['15-over'].append(i)
+            mrr_groups['15-over'].append(i)
+            ndcg_groups['15-over'].append(i)
     
+    # hit rate mean
     mean_hit_rate = np.mean(hit_rates[hit_rates>-1])
-    group_0_5_hit_rate = np.mean(hit_rates[group_0_5][hit_rates[group_0_5]>-1])
-    group_5_10_hit_rate = np.mean(hit_rates[group_5_10][hit_rates[group_5_10]>-1])
-    group_10_15_hit_rate = np.mean(hit_rates[group_10_15][hit_rates[group_10_15]>-1])
-    group_15_over_hit_rate = np.mean(hit_rates[group_15_over][hit_rates[group_15_over]>-1])
+    for group_name, group_indices in hit_rate_groups.items():
+        group_hit_rate[group_name] = np.mean(hit_rates[group_indices][hit_rates[group_indices]>-1])
+
+    # mrr mean
+    mean_mrr = np.mean(mrr[mrr>-1])
+    for group_name, group_indices in mrr_groups.items():
+        group_mrr[group_name] = np.mean(mrr[group_indices][mrr[group_indices]>-1])
+
+    # nDCG mean
+    mean_ndcg = np.mean(ndcg[ndcg>-1])
+    for group_name, group_indices in ndcg_groups.items():
+        group_ndcg[group_name] = np.mean(ndcg[group_indices][ndcg[group_indices]>-1])
 
     return (
         mean_hit_rate,
-        group_0_5_hit_rate,
-        group_5_10_hit_rate,
-        group_10_15_hit_rate,
-        group_15_over_hit_rate
+        mean_mrr,
+        mean_ndcg,
+        group_hit_rate,
+        group_mrr,
+        group_ndcg
     )
 
 def evaluate(rating_matrix):
@@ -49,8 +109,7 @@ def evaluate(rating_matrix):
     test_data = np.zeros(rating_matrix.shape)
 
     for i, interaction in enumerate(rating_matrix):
-        up_index = np.where(interaction > 0)[0]
-        interaction_count = len(up_index)
+        interaction_count = len(interaction)
 
         # sample data for train and test with the ratio of 60:40
         train_number = round(interaction_count * 0.6)
@@ -62,39 +121,50 @@ def evaluate(rating_matrix):
         train_mask = sample_indexes==0
         test_mask = sample_indexes==1
 
-        test_data[i][up_index[test_mask]] = interaction[up_index[test_mask]]
-        train_data[i][up_index[train_mask]] = interaction[up_index[train_mask]]
+        test_data[i][test_mask] = interaction[test_mask]
+        train_data[i][train_mask] = interaction[train_mask]
     
     top_k = [10, 15, 20]
     training_results = []
+    result_title_str = 'top %s, hit rate: %.3f, MRR: %.3f, nDCG: %.3f'
+    group_title_str = ''
 
     for k in top_k:
         (
             mean_hit_rate,
-            group_0_5_hit_rate,
-            group_5_10_hit_rate,
-            group_10_15_hit_rate,
-            group_15_over_hit_rate
+            mean_mrr,
+            mean_ndcg,
+            group_hit_rate,
+            group_mrr,
+            group_ndcg
         ) = top_k_evaluate(k, rating_matrix, train_data, test_data)
 
-        training_results.append([
-            k, 
+        result = [
+            k,
             mean_hit_rate,
-            group_0_5_hit_rate,
-            group_5_10_hit_rate,
-            group_10_15_hit_rate,
-            group_15_over_hit_rate
-        ])
+            mean_mrr,
+            mean_ndcg
+        ]
+
+        for name, value in group_hit_rate.items():
+            result.append(value)
+            if k == top_k[0]:
+                group_title_str += ', Hit rate group ' + name + ': %.3f'
+
+        for name, value in group_mrr.items():
+            result.append(value)
+            if k == top_k[0]:
+                group_title_str += ', MRR group ' + name + ': %.3f'
+
+        for name, value in group_ndcg.items():
+            result.append(value)
+            if k == top_k[0]:
+                group_title_str += ', nDCG group ' + name + ': %.3f'
+
+        training_results.append(result)
 
     for result in training_results:
-        print('hit rate for top %s: %.3f, Group 0 to 5: %.3f, Group 5 to 10: %.3f, Group 10 to 15: %.3f, Group 15 to 20: %.3f' % (
-            result[0], 
-            result[1],
-            result[2],
-            result[3],
-            result[4],
-            result[5]
-        ))
+        print((result_title_str + group_title_str) % tuple(result))
 
 if __name__ == "__main__":
     interaction_matrix = pickle.load(open('./data/interaction_matrix.p', 'rb'))
